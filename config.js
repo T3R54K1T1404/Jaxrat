4d03a5ae5df9c059ebfd47a1b9b81dd0f24bce4f
T3R54K1T1404 <44319677+T3R54K1T1404@users.noreply.github.com>
2026-03-15 04:58:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+// Firebase Configuration - JAXRAT V5
+const firebaseConfig = {
+  apiKey: "AIzaSyDzA92c3IoyjhiFft3Ci6cpsvEJBXAzIfQ",
+  authDomain: "jaxrat-6813c.firebaseapp.com",
+  databaseURL: "https://jaxrat-6813c-default-rtdb.firebaseio.com",
+  projectId: "jaxrat-6813c",
+  storageBucket: "jaxrat-6813c.firebasestorage.app",
+  messagingSenderId: "750985295231",
+  appId: "1:750985295231:web:0679d4b998e52256e4d64a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
+
+// Global Variables
+let currentUser = null;
+let ownerUid = null;
